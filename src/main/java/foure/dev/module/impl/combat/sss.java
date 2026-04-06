9264a796970ef055e9d2ffe9777beae84ee5cc4b
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-04-06 21:27:06 +0300

    Update module description for 'sss'

---
+++ src/main/java/foure/dev/module/impl/combat/sss.java
@@ -0,0 +1,24 @@
+package foure.dev.module.impl.combat;
+
+import foure.dev.module.api.Category;
+import foure.dev.module.api.Function;
+import foure.dev.module.api.ModuleInfo;
+import foure.dev.module.setting.api.Setting;
+import foure.dev.module.setting.impl.BooleanSetting;
+import foure.dev.module.setting.impl.ModeSetting;
+import foure.dev.module.setting.impl.NumberSetting;
+
+@ModuleInfo(
+   name = "sss",
+   category = Category.COMBAT,
+   desc = "sss"
+)
+public class sss extends Function {
+   private final BooleanSetting rotate = new BooleanSetting("Rotate", this, true);
+   private final NumberSetting range = new NumberSetting("Range", (Function)null, 3.0D, 1.0D, 6.0D, 0.1D);
+   private final ModeSetting mode = new ModeSetting("Mode", this, "Switch", new String[]{"Switch", "Single", "Multi"});
+
+   public sss() {
+      this.addSettings(new Setting[]{this.rotate, this.range, this.mode});
+   }
+}
