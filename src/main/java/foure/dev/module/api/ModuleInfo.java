5ff5a75ecfe8430e568c971ba922f6f63c45f1b5
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-04-13 12:35:34 +0300

    Update ModuleInfo.java

--- src/main/java/foure/dev/module/api/ModuleInfo.java
+++ src/main/java/foure/dev/module/api/ModuleInfo.java
@@ -6,10 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ModuleInfo {
    String name();
-
    String desc() default "";
-
    Category category();
-
    boolean visual() default false;
+   String icon() default ""; // calea spre iconita in assets
 }
