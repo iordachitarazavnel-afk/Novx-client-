a07aac358225b6fda76ddb0e80f466e050febeb6
iordachitarazavnel-afk <iordachitarazavnel@gmail.com>
2026-04-18 11:36:01 +0300

    Update RenderUtils.java

--- src/main/java/foure/dev/util/render/RenderUtils.java
+++ src/main/java/foure/dev/util/render/RenderUtils.java
@@ -1,14 +1,8 @@
 package foure.dev.util.render;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.ShaderProgram;
-import net.minecraft.client.gl.ShaderProgramKeys;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.render.BufferRenderer;
-import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.BufferAllocator;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Box;
 import org.joml.Matrix4f;
@@ -16,12 +10,7 @@ import org.joml.Matrix4f;
 import java.awt.Color;
 
 /**
- * RenderUtils for MC 1.21.4+ / Yarn 1.21.4+
- *
- * Changes vs older versions:
- *  - RenderSystem.enableBlend/disableBlend/defaultBlendFunc/enableDepthTest/disableDepthTest removed
- *  - VertexFormat moved to net.minecraft.client.render.VertexFormat (DrawMode is inner class)
- *  - BufferRenderer.drawWithGlobalProgram -> BufferRenderer.draw
+ * RenderUtils for MC 1.21.11 / Yarn 1.21.11+build.2
  */
 public class RenderUtils {
 
@@ -35,104 +24,89 @@ public class RenderUtils {
                                        float x1, float y1, float z1,
                                        float x2, float y2, float z2,
                                        Color color) {
-        float r = color.getRed()   / 255.0f;
-        float g = color.getGreen() / 255.0f;
-        float b = color.getBlue()  / 255.0f;
-        float a = color.getAlpha() / 255.0f;
-
-        Tessellator tess = Tessellator.getInstance();
+        int argb = color.getRGB();
         Matrix4f mat = matrices.peek().getPositionMatrix();
 
-        var buf = tess.begin(
-            VertexFormat.DrawMode.QUADS,
-            VertexFormats.POSITION_COLOR);
+        try (BufferAllocator alloc = new BufferAllocator(1024)) {
+            VertexConsumerProvider.Immediate vcp = VertexConsumerProvider.immediate(alloc);
+            VertexConsumer vc = vcp.getBuffer(RenderLayer.getDebugFilledBox());
 
-        // bottom
-        buf.vertex(mat, x1,y1,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z2).color(r,g,b,a);
-        buf.vertex(mat, x1,y1,z2).color(r,g,b,a);
-        // top
-        buf.vertex(mat, x1,y2,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z1).color(r,g,b,a);
-        buf.vertex(mat, x1,y2,z1).color(r,g,b,a);
-        // north
-        buf.vertex(mat, x1,y2,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z1).color(r,g,b,a);
-        buf.vertex(mat, x1,y1,z1).color(r,g,b,a);
-        // south
-        buf.vertex(mat, x1,y1,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z2).color(r,g,b,a);
-        buf.vertex(mat, x1,y2,z2).color(r,g,b,a);
-        // west
-        buf.vertex(mat, x1,y1,z1).color(r,g,b,a);
-        buf.vertex(mat, x1,y1,z2).color(r,g,b,a);
-        buf.vertex(mat, x1,y2,z2).color(r,g,b,a);
-        buf.vertex(mat, x1,y2,z1).color(r,g,b,a);
-        // east
-        buf.vertex(mat, x2,y2,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z2).color(r,g,b,a);
-        buf.vertex(mat, x2,y1,z1).color(r,g,b,a);
+            vc.vertex(mat,x1,y1,z2).color(argb);
+            vc.vertex(mat,x2,y1,z2).color(argb);
+            vc.vertex(mat,x2,y2,z2).color(argb);
+            vc.vertex(mat,x1,y2,z2).color(argb);
 
-        BufferRenderer.draw(buf.end());
+            vc.vertex(mat,x2,y1,z1).color(argb);
+            vc.vertex(mat,x1,y1,z1).color(argb);
+            vc.vertex(mat,x1,y2,z1).color(argb);
+            vc.vertex(mat,x2,y2,z1).color(argb);
+
+            vc.vertex(mat,x1,y1,z1).color(argb);
+            vc.vertex(mat,x1,y1,z2).color(argb);
+            vc.vertex(mat,x1,y2,z2).color(argb);
+            vc.vertex(mat,x1,y2,z1).color(argb);
+
+            vc.vertex(mat,x2,y1,z2).color(argb);
+            vc.vertex(mat,x2,y1,z1).color(argb);
+            vc.vertex(mat,x2,y2,z1).color(argb);
+            vc.vertex(mat,x2,y2,z2).color(argb);
+
+            vc.vertex(mat,x1,y2,z2).color(argb);
+            vc.vertex(mat,x2,y2,z2).color(argb);
+            vc.vertex(mat,x2,y2,z1).color(argb);
+            vc.vertex(mat,x1,y2,z1).color(argb);
+
+            vc.vertex(mat,x1,y1,z1).color(argb);
+            vc.vertex(mat,x2,y1,z1).color(argb);
+            vc.vertex(mat,x2,y1,z2).color(argb);
+            vc.vertex(mat,x1,y1,z2).color(argb);
+
+            vcp.draw();
+        }
     }
 
     public static void renderBoxOutline(MatrixStack matrices,
                                         float x1, float y1, float z1,
                                         float x2, float y2, float z2,
                                         Color color) {
-        float r = color.getRed()   / 255.0f;
-        float g = color.getGreen() / 255.0f;
-        float b = color.getBlue()  / 255.0f;
-        float a = color.getAlpha() / 255.0f;
-
-        Tessellator tess = Tessellator.getInstance();
+        int argb = color.getRGB();
         Matrix4f mat = matrices.peek().getPositionMatrix();
 
-        var buf = tess.begin(
-            VertexFormat.DrawMode.DEBUG_LINES,
-            VertexFormats.POSITION_COLOR);
+        try (BufferAllocator alloc = new BufferAllocator(1024)) {
+            VertexConsumerProvider.Immediate vcp = VertexConsumerProvider.immediate(alloc);
+            VertexConsumer vc = vcp.getBuffer(RenderLayer.getLines());
 
-        buf.vertex(mat,x1,y1,z1).color(r,g,b,a); buf.vertex(mat,x2,y1,z1).color(r,g,b,a);
-        buf.vertex(mat,x2,y1,z1).color(r,g,b,a); buf.vertex(mat,x2,y1,z2).color(r,g,b,a);
-        buf.vertex(mat,x2,y1,z2).color(r,g,b,a); buf.vertex(mat,x1,y1,z2).color(r,g,b,a);
-        buf.vertex(mat,x1,y1,z2).color(r,g,b,a); buf.vertex(mat,x1,y1,z1).color(r,g,b,a);
-        buf.vertex(mat,x1,y2,z1).color(r,g,b,a); buf.vertex(mat,x2,y2,z1).color(r,g,b,a);
-        buf.vertex(mat,x2,y2,z1).color(r,g,b,a); buf.vertex(mat,x2,y2,z2).color(r,g,b,a);
-        buf.vertex(mat,x2,y2,z2).color(r,g,b,a); buf.vertex(mat,x1,y2,z2).color(r,g,b,a);
-        buf.vertex(mat,x1,y2,z2).color(r,g,b,a); buf.vertex(mat,x1,y2,z1).color(r,g,b,a);
-        buf.vertex(mat,x1,y1,z1).color(r,g,b,a); buf.vertex(mat,x1,y2,z1).color(r,g,b,a);
-        buf.vertex(mat,x2,y1,z1).color(r,g,b,a); buf.vertex(mat,x2,y2,z1).color(r,g,b,a);
-        buf.vertex(mat,x2,y1,z2).color(r,g,b,a); buf.vertex(mat,x2,y2,z2).color(r,g,b,a);
-        buf.vertex(mat,x1,y1,z2).color(r,g,b,a); buf.vertex(mat,x1,y2,z2).color(r,g,b,a);
+            vc.vertex(mat,x1,y1,z1).color(argb); vc.vertex(mat,x2,y1,z1).color(argb);
+            vc.vertex(mat,x2,y1,z1).color(argb); vc.vertex(mat,x2,y1,z2).color(argb);
+            vc.vertex(mat,x2,y1,z2).color(argb); vc.vertex(mat,x1,y1,z2).color(argb);
+            vc.vertex(mat,x1,y1,z2).color(argb); vc.vertex(mat,x1,y1,z1).color(argb);
+            vc.vertex(mat,x1,y2,z1).color(argb); vc.vertex(mat,x2,y2,z1).color(argb);
+            vc.vertex(mat,x2,y2,z1).color(argb); vc.vertex(mat,x2,y2,z2).color(argb);
+            vc.vertex(mat,x2,y2,z2).color(argb); vc.vertex(mat,x1,y2,z2).color(argb);
+            vc.vertex(mat,x1,y2,z2).color(argb); vc.vertex(mat,x1,y2,z1).color(argb);
+            vc.vertex(mat,x1,y1,z1).color(argb); vc.vertex(mat,x1,y2,z1).color(argb);
+            vc.vertex(mat,x2,y1,z1).color(argb); vc.vertex(mat,x2,y2,z1).color(argb);
+            vc.vertex(mat,x2,y1,z2).color(argb); vc.vertex(mat,x2,y2,z2).color(argb);
+            vc.vertex(mat,x1,y1,z2).color(argb); vc.vertex(mat,x1,y2,z2).color(argb);
 
-        BufferRenderer.draw(buf.end());
+            vcp.draw();
+        }
     }
 
     public static void drawLine(MatrixStack matrices,
                                 float x1, float y1, float z1,
                                 float x2, float y2, float z2,
                                 Color color) {
-        float r = color.getRed()   / 255.0f;
-        float g = color.getGreen() / 255.0f;
-        float b = color.getBlue()  / 255.0f;
-        float a = color.getAlpha() / 255.0f;
-
-        Tessellator tess = Tessellator.getInstance();
+        int argb = color.getRGB();
         Matrix4f mat = matrices.peek().getPositionMatrix();
 
-        var buf = tess.begin(
-            VertexFormat.DrawMode.DEBUG_LINES,
-            VertexFormats.POSITION_COLOR);
-
-        buf.vertex(mat, x1,y1,z1).color(r,g,b,a);
-        buf.vertex(mat, x2,y2,z2).color(r,g,b,a);
-
-        BufferRenderer.draw(buf.end());
+        try (BufferAllocator alloc = new BufferAllocator(128)) {
+            VertexConsumerProvider.Immediate vcp = VertexConsumerProvider.immediate(alloc);
+            VertexConsumer vc = vcp.getBuffer(RenderLayer.getLines());
+            vc.vertex(mat,x1,y1,z1).color(argb);
+            vc.vertex(mat,x2,y2,z2).color(argb);
+            vcp.draw();
+        }
     }
 
     public static void renderFilledBox(MatrixStack matrices, Box box, Color color) {
